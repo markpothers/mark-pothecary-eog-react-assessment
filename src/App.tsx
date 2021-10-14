2baca6d73a66fb3e4b84144a0a6fdd2fdcf9bb00
Mark Pothecary <markpothers@hotmail.com>
2021-10-13 21:10:23 -0500

    setup and rudimentary select in a new card implemented

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
 import Wrapper from './components/Wrapper';
 import NowWhat from './components/NowWhat';
+import ChartDisplay from './components/ChartDisplay';
 
 const theme = createTheme({
   palette: {
@@ -26,6 +27,7 @@ const App = () => (
     <CssBaseline />
     <Wrapper>
       <Header />
+      <ChartDisplay />
       <NowWhat />
       <ToastContainer />
     </Wrapper>
---
+++ src/components/ChartDisplay.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+import CardContent from '@material-ui/core/CardContent';
+// import List from '@material-ui/core/List';
+// import ListItem from '@material-ui/core/ListItem';
+// import ListItemText from '@material-ui/core/ListItemText';
+import { makeStyles } from '@material-ui/core/styles';
+import CardHeader from './CardHeader';
+import InputSelect from './InputSelect';
+// import Avatar from './Avatar';
+
+const useStyles = makeStyles({
+  card: {
+    margin: '5% 10%',
+  },
+});
+
+export default () => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.card}>
+      <CardHeader title="Select any parameter in the dropdown menu to display its recent history on the chart" />
+      <CardContent>
+        <InputSelect />
+        <Typography variant="body1">
+          We hope the data is useful!
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
